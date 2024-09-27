88fa9f5e42afeeb826ca8623f87f18d8f01da39f
Sava Gerov <sgerov@Savas-MacBook-Pro.local>
2024-09-27 20:57:48 +0800

    change url shape

--- models_server/urls.py
+++ models_server/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('predictor.urls')),  # Include the predictor app URLs
+    path('v1/', include('predictor.urls')),  # Include the predictor app URLs
 ]
