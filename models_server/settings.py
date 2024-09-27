fea9f577b157d1207ddb615cac6f38d9eeef1268
Sava Gerov <sgerov@Savas-MacBook-Pro.local>
2024-09-27 22:42:05 +0800

    adjust max sizeg

--- models_server/settings.py
+++ models_server/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE=10485760
+FILE_UPLOAD_MAX_MEMORY_SIZE=10485760
